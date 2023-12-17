27988b17e65cb5d507fede4c3aec7b893d9651f8
adhishrayas <adhishrayas@gmail.com>
2023-12-17 22:44:51 +0530

    added all host

--- techsnap/settings.py
+++ techsnap/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-55b-47mkye==&lk-vk!o3&0zzz8!thrc9d@%i^=wrtr#b!4$6&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
